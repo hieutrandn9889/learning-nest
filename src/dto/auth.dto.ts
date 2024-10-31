4a72fe304aa8599266cb3231a76b40137850a924
hieutrandn9889 <hieutrandn123@gmail.com>
2024-11-01 05:42:09 +0700

    auth dto for signIn and signUp

---
+++ src/dto/auth.dto.ts
@@ -0,0 +1,28 @@
+export class AuthResponseDto {
+    id?: number;
+    username?: string;
+    permission?: string;
+    constructor({ id, username, permission }) {
+        this.id = id;
+        this.username = username;
+        this.permission = permission ;
+        return this;
+    };
+}
+
+export class AuthPayloadDto{
+    username: string;
+    password: string;
+}
+
+export class AuthPermission{
+    id?: number;
+    token?: string;
+    expiredTime?: string;
+    constructor({ id, token, expiredTime }) {
+        this.id = id;
+        this.token = token;
+        this.expiredTime = expiredTime ;
+        return this;
+    };
+}
